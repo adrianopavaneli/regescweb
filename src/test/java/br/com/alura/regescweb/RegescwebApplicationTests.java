9c01f4f44dc0a542c1ee118fdfc95eb4ddf40202
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-11-21 22:25:43 -0300

    first commit

---
+++ src/test/java/br/com/alura/regescweb/RegescwebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.regescweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegescwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
